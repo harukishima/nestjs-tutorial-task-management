01e576cb18fefdc04723d8eb3bd56eb423d969fa
HaruKishima <18120256@student.hcmus.edu.vn>
2021-12-19 10:10:05 +0700

    add error handling and validation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -29,7 +29,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((item) => item.id === id);
+    const task = this.tasks.find((item) => item.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -45,7 +49,8 @@ export class TasksService {
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((item) => item.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((item) => item.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
