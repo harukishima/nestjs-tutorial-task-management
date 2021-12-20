d86bd4f5f8c01b0c62251b8e694e09acc72e495b
HaruKishima <18120256@student.hcmus.edu.vn>
2021-12-20 17:30:30 +0700

    apply logging

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,9 +4,12 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './tasks-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from '../auth/user.entity';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository', { timestamp: true });
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -22,8 +25,17 @@ export class TasksRepository extends Repository<Task> {
         { search: `%${search.toLowerCase()}%` },
       );
     }
-
-    return query.getMany();
+    try {
+      return query.getMany();
+    } catch (err) {
+      this.logger.error(
+        `Failed to get tasks for user ${
+          user.username
+        }. Filters: ${JSON.stringify(filterDto)}`,
+        err.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
